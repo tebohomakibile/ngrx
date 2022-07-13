a55dbf4bc0118f3f8e9030f6f9a804217c9e5802
Teboho Makibile <Teboho.Makibile@multichoice.co.za>
2022-07-13 10:59:17 +0200

    Created a reducer, initialsed the state for the product list module

---
+++ src/app/products/state/product.reducer.ts
@@ -0,0 +1,15 @@
+import { createAction, createReducer, on } from "@ngrx/store";
+
+export const productReducer = createReducer(
+  // Specify the initial store state
+  {showProductCode: true},
+  //Define an action for the reducer to dispatch
+  on(createAction('[Product] Toggle Product Code'), state => {
+    console.log('Original State', JSON.stringify(state));
+    
+    return {
+      ...state,
+      showProductCode: !state.showProductCode
+    }
+  })
+)
